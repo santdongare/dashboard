a08269dd5d375bb26161297a166ee5b9beffa171
santosh <santdongare1994@gmail.com>
2021-11-14 22:22:48 +0530

    seventh commit

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { TDashService } from '../tdash.service';
 import { Router,  ActivatedRoute } from '@angular/router';
-
+import { IDropdownSettings } from 'ng-multiselect-dropdown';
+import {ngxCsv} from 'ngx-csv/ngx-csv'
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
@@ -9,16 +10,73 @@ import { Router,  ActivatedRoute } from '@angular/router';
 })
 export class TableComponent implements OnInit {
   search!: string;
-  obj:any
+  obj:any;
   Name:any;
   page: number = 1;
-  constructor(private router:Router,private ActivateedRoute:ActivatedRoute,public TDash:TDashService) { }
+  dropdownList:any = [];
+  dropdownSettings!: {};
+  tmp:any={};
+  display=true;
+  constructor(private router:Router,private ActivateedRoute:ActivatedRoute,private TDash:TDashService) 
+  { 
+       this.TDash.getUserData().subscribe(data=>{
+       console.log(data);
+       this.obj=data;
+    })
 
+    
+  
+    // this.TDash.getData().subscribe((data:any)=>{
+    //   console.log(data);
+    //  this.tmp=data;
+    // })
+  }
+  toggle(){
+      this.display=!this.display;
+  }
+  filedownload(){
+    var options = { 
+      fieldSeparator: ',',
+      quoteStrings: '"',
+      decimalseparator: '.',
+      showLabels: true, 
+      showTitle: true,
+      title: 'Report Data',
+      useBom: true,
+      noDownload: false,
+      headers: ["projectType", "projectID", "IprojectTitle","commodityType","projectOwner","Business"]
+     
+    };
+   
+    new ngxCsv(this.obj, "Report", options);
+  }
+
+    
   ngOnInit(): void {
 
-    this.obj = this.TDash.DashData;
+    
+    
+     //this.dropdownList = this.tmp;
+    //   { item_id: 1, item_text: 'Mumbai' },
+    //   { item_id: 2, item_text: 'Kolhapur' },
+    //   { item_id: 3, item_text: 'Pune' },
+    //   { item_id: 4, item_text: 'Nagpur' },
+    //   { item_id: 5, item_text: 'Nashik' }
+    // ];
+    
+    // this.dropdownSettings = {
+    //   singleSelection: false,
+    //   idField: 'item_id',
+    //   textField: 'item_text',
+    //   selectAllText: 'Select All',
+    //   unSelectAllText: 'UnSelect All',
+    //   itemsShowLimit: 5,
+    //   allowSearchFilter: true
+    //};
     
    
   }
+  
 
 }
+
